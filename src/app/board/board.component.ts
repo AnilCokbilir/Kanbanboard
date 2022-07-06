9145a8380b8c9f25200e2c90888b31813da0ba7d
Anil Cokbilir <anil-27@hotmail.de>
2022-07-06 12:34:48 +0200

    json filter for drag&drop

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { AlltasksService } from '../alltasks.service';
 import { Firestore, collectionData, collection, setDoc, doc } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { filter, map, Observable } from 'rxjs';
+import { Xliff } from '@angular/compiler';
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
@@ -11,23 +13,31 @@ import { Observable } from 'rxjs';
 export class BoardComponent implements OnInit {
   tasks$: Observable<any>;
   allTasksFire: any[] = [];
-
+  done: any;
+  doneList: any;
+  todoList: any;
+  todo: any;
+  DragTodo: any;
+  test: any;
+  testtwo: any;
   constructor(public alltasks: AlltasksService, private firestore: Firestore) {
+
     const coll: any = collection(firestore, 'tasks');
     this.tasks$ = collectionData(coll);
 
     this.tasks$.subscribe((newTasks) => {
       console.log('Neue Tasks:', newTasks)
-
+      this.allTasksFire = newTasks;
+      this.test = this.allTasksFire.filter(t => t.object.Urgency == 'Medium')
+      console.log('array', this.allTasksFire)
+      console.log(this.test)
     })
   }
 
   ngOnInit(): void {
   }
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-  drop(event: CdkDragDrop<any[]>) {
+  drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -39,4 +49,6 @@ export class BoardComponent implements OnInit {
       );
     }
   }
+
+
 }
