f089bca767619bcc2fded40715c5e4c5d1902c28
Anil Cokbilir <anil-27@hotmail.de>
2022-10-22 12:01:09 +0200

    logIn and header fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { AlltasksService } from '../alltasks.service';
 
 @Component({
   selector: 'app-login',
@@ -9,25 +10,30 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 export class LoginComponent implements OnInit {
   email: any;
   password: any;
-  constructor() { }
+  constructor(private allTasksService: AlltasksService) { }
 
   ngOnInit(): void {
 
   }
 
   log() {
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, this.email, this.password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
+    setTimeout(() => {
+
+
+      this.allTasksService.loggedIn = true;
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, this.email, this.password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
+    }, 100);
   }
 
 }
